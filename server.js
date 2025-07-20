aa1880f731ea75c8b47cb3eb89d7b4df017af6c7
Muhammad Atif <ranaatif1299@gmail.com>
2025-07-20 21:25:15 +0500

    Updated version

--- server.js
+++ server.js
@@ -60,7 +60,4 @@ app.use('/showcase', showcaseRoutes)
 app.use('/blog', blogRoutes)
 app.use('/api', bookingRoutes) // Mount booking routes under /api
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+module.exports = app
